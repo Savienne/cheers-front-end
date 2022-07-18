da8a7a01068f71ea04a5320a215c40bc81713bcf
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-18 11:14:06 -0400

    working on boozy-tune page

--- src/pages/BoozyTunes/BoozyTunes.jsx
+++ src/pages/BoozyTunes/BoozyTunes.jsx
@@ -1,12 +1,35 @@
 const BoozyTunes= (props) => {
+    let poll = {
+        
+        question:"Does this song go with this drink?",
+        answers: [
+            "Yes", "No"
+        ],
+        pollCount:20,
+        answersWeight:[4, 4, 2, 10],
+        selectedAnswer: -1
+
+    };
+    let pollDOM = {
+        question:document.querySelector("poll question"),
+        answers:document.querySelector("poll answers"),
+
+        pollDOM.question.
+
+
+    };
+
     return ( 
+        
       <>
       <div>
       <h1>Boozy Tunes</h1>
       <h2> Boozy Tunes Results</h2>
       <h3> 60% of users said YES, THIS SONG goes with THIS DRINK</h3>
-      <label htmlFor="Poll" className={poll}>Poll</label>
-
+    <div class="poll">
+    <div class="question"></div>
+    <div class="answers"></div>
+    </div>
 
       <button>Continue</button> <br></br><button>Cancel</button>
 
