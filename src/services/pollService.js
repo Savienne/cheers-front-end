57975448d70b445b4ed407975034c6de24d2d3cd
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-20 12:02:02 -0400

    poll progress

--- src/services/pollService.js
+++ src/services/pollService.js
@@ -1,9 +1,11 @@
 import * as tokenService from './tokenService'
 
-const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/poll`
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}`
 
 async function create(poll) {
+
   const res = await fetch(BASE_URL, {
+
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${tokenService.getToken()}`,
@@ -14,48 +16,7 @@ async function create(poll) {
   return res.json()
 }
 
-async function getAll() {
-  const res = await fetch(BASE_URL)
-  return res.json()
-}
-
-async function deleteOne(id) {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`
-    }
-  })
-  return res.json()
-}
-
-async function update(poll) {
-  const res = await fetch(`${BASE_URL}/${poll._id}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(poll)
-  })
-  return res.json()
-}
-
-async function addVote(voteData, pollId) {
-  const res = await fetch(`${BASE_URL}/${pollId}/add-vote`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`
-    },
-    body: voteData
-  })
-  return await res.json()
-}
 
 export {
   create,
-  getAll,
-  deleteOne,
-  update,
-  addVote
 }
